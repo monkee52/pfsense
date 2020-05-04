047cd8c3b3c28cd4aad05a2fbb8c3f4f47de434d
Ayden Hull <ayden.hull@internode.on.net>
2020-05-04 14:34:14 +1000

    Create VXLAN interfaces

--- src/usr/local/www/interfaces_vxlan_edit.php
+++ src/usr/local/www/interfaces_vxlan_edit.php
@@ -38,11 +38,75 @@ $id = $_REQUEST['id'];
 if (isset($id) && $a_vxlans[$id]) {
 	$pconfig['if'] = $a_vxlans[$id]['if'];
 	$pconfig['vxlanif'] = $a_vxlans[$id]['vxlanif'];
+	$pconfig['remote-addr'] = $a_vxlans[$id]['remote-addr'];
+	$pconfig['remote-port'] = $a_vxlans[$id]['remote-port'];
+	$pconfig['vni'] = $a_vxlans[$id]['vni'];
 	$pconfig['descr'] = $a_vxlans[$id]['descr'];
 }
 
 if ($_POST['save']) {
 
+	unset($input_errors);
+	$pconfig = $_POST;
+
+	$pconfig['remote-addr'] = addrtolower($_POST['remote-addr']);
+
+	/* input validation */
+	$reqdfields = explode(" ", "if remote-addr remote-port vni");
+	$reqdfieldsn = array(gettext("Parent interface"), gettext("Remote tunnel IP address"), gettext("Remote tunnel identifier"));
+
+	do_input_validation($_POST, $reqdfields, $reqdfieldsn, $input_errors);
+
+	if (!is_ipaddr($_POST['remote-addr'])) {
+		$input_errors[] = gettext("The remote address field must have valid IP addresses.");
+	}
+
+	if (!is_numericint($_POST['remote-port'])) {
+		$input_errors[] = gettext("The remote port field must be an integer.");
+	}
+
+	foreach ($a_vxlans as $vxlan) {
+		if (isset($id) && ($a_vxlans[$id]) && ($a_vxlans[$id] === $vxlan)) {
+			continue;
+		}
+
+		if (($vxlan['id'] == $_POST['id']) && ($vxlan['remote-addr'] == $_POST['remote-addr']) && ($vxlan['remote-port'] == $_POST['remote-port']) && ($vxlan['vni'] == $_POST['vni'])) {
+			$input_errors[] = sprintf(gettext("A VXLAN tunnel with the %s:%s (VNI: %s) is already defined."), $vxlan['remote-addr'], $vxlan['remote-port'], $vxlan['vni']);
+			break;
+		}
+	}
+
+	if (!$input_errors) {
+		$vxlan = array();
+		$vxlan['if'] = $_POST['if'];
+		$vxlan['remote-addr'] = $_POST['remote-addr'];
+		$vxlan['remote-port'] = $_POST['remote-port'];
+		$vxlan['vni'] = $_POST['vni'];
+		$vxlan['descr'] = $_POST['descr'];
+		$vxlan['vxlanif'] = $_POST['vxlanif'];
+
+		$vxlan['vxlanif'] = interface_vxlan_configure($vxlan);
+		if ($vxlan['vxlanif'] == "" || !stristr($vxlan['vxlanif'], "vxlan")) {
+			$input_errors[] = gettext("Error occured creating interface, please retry.");
+		} else {
+			if (isset($id) && $a_vxlans[$id]) {
+				$a_vxlans[$id] = $vxlan;
+			} else {
+				$a_vxlans[] = $vxlan;
+			}
+
+			write_config();
+
+			$confif = convert_real_interface_to_friendly_interface_name($vxlan['vxlanif']);
+
+			if ($confif != "") {
+				interface_configure($confif);
+			}
+
+			header("Location: interfaces_vxlan.php");
+			exit;
+		}
+	}
 }
 
 function build_parent_list() {
@@ -79,7 +143,23 @@ $section->addInput(new Form_IpAddress(
 	'remote-addr',
 	'*VXLAN Remote Address',
 	$pconfig['remote-addr']
-))->setHelp('Peer address where VXLAN packets will be sent.');
+))->setHelp('The IP address of the remote tunnel endpoint, or the IP multicast group address to join.');
+
+$section->addInput(new Form_Input(
+	'remote-port',
+	'VXLAN Remote Port',
+	'number',
+	$pconfig['remote-port'] ?: 4789,
+	['min' => 0]
+))->setHelp('The destination port number used in the encapsulating IPv4/IPv6 header.');
+
+$section->addInput(new Form_Input(
+	'vni',
+	'VNI',
+	'number',
+	$pconfig['vni'],
+	['min' => 0]
+))->setHelp('This value is a 24-bit VXLAN Network Identifier (VNI) that identifies the virtual network segment membership of the interface.');
 
 $section->addInput(new Form_Input(
 	'descr',
